ddbc43c2400db6005c9d2c2163c12e785c49d359
Manish R Jain <manishrjain@gmail.com>
2022-07-04 13:53:06 -0700

    Gutted txn out. It is now a read-only txn

--- txn.go
+++ txn.go
@@ -20,45 +20,19 @@ import (
 	"bytes"
 	"encoding/hex"
 	"math"
-	"strconv"
-	"sync"
 	"sync/atomic"
 
 	"github.com/outcaste-io/badger/v3/y"
-	"github.com/outcaste-io/ristretto/z"
 	"github.com/pkg/errors"
 )
 
 // Txn represents a Badger transaction.
 type Txn struct {
-	readTs   uint64
-	commitTs uint64
-	size     int64
-	count    int64
-	db       *DB
-
-	reads []uint64 // contains fingerprints of keys read.
-	// contains fingerprints of keys written. This is used for conflict detection.
-	conflictKeys map[uint64]struct{}
-	readsLock    sync.Mutex // guards the reads slice. See addReadKey.
-
-	pendingWrites   map[string]*Entry // cache stores any writes done by txn.
-	duplicateWrites []*Entry          // Used in managed mode to store duplicate entries.
+	readTs uint64
+	db     *DB
 
 	numIterators int32
 	discarded    bool
-	update       bool // update is used to conditionally keep track of reads.
-}
-
-func (txn *Txn) checkSize(e *Entry) error {
-	count := txn.count + 1
-	// Extra bytes for the version in key.
-	size := txn.size + e.estimateSize() + 10
-	if count >= txn.db.opt.maxBatchCount || size >= txn.db.opt.maxBatchSize {
-		return ErrTxnTooBig
-	}
-	txn.count, txn.size = count, size
-	return nil
 }
 
 func exceedsSize(prefix string, max int64, key []byte) error {
@@ -89,82 +63,6 @@ func ValidEntry(db *DB, key, val []byte) error {
 	return nil
 }
 
-func (txn *Txn) modify(e *Entry) error {
-	switch {
-	case !txn.update:
-		return ErrReadOnlyTxn
-	case txn.discarded:
-		return ErrDiscardedTxn
-	case len(e.Key) == 0:
-		return ErrEmptyKey
-	case bytes.HasPrefix(e.Key, badgerPrefix):
-		return ErrInvalidKey
-	case len(e.Key) > maxKeySize:
-		// Key length can't be more than uint16, as determined by table::header.  To
-		// keep things safe and allow badger move prefix and a timestamp suffix, let's
-		// cut it down to 65000, instead of using 65536.
-		return exceedsSize("Key", maxKeySize, e.Key)
-	case txn.db.opt.InMemory && int64(len(e.Value)) > maxValueThreshold:
-		return exceedsSize("Value", maxValueThreshold, e.Value)
-	}
-
-	if err := txn.db.isBanned(e.Key); err != nil {
-		return err
-	}
-	if err := txn.checkSize(e); err != nil {
-		return err
-	}
-
-	// The txn.conflictKeys is used for conflict detection. If conflict detection
-	// is disabled, we don't need to store key hashes in this map.
-	if txn.db.opt.DetectConflicts {
-		fp := z.MemHash(e.Key) // Avoid dealing with byte arrays.
-		txn.conflictKeys[fp] = struct{}{}
-	}
-	// If a duplicate entry was inserted in managed mode, move it to the duplicate writes slice.
-	// Add the entry to duplicateWrites only if both the entries have different versions. For
-	// same versions, we will overwrite the existing entry.
-	if oldEntry, ok := txn.pendingWrites[string(e.Key)]; ok && oldEntry.version != e.version {
-		txn.duplicateWrites = append(txn.duplicateWrites, oldEntry)
-	}
-	txn.pendingWrites[string(e.Key)] = e
-	return nil
-}
-
-// Set adds a key-value pair to the database.
-// It will return ErrReadOnlyTxn if update flag was set to false when creating the transaction.
-//
-// The current transaction keeps a reference to the key and val byte slice
-// arguments. Users must not modify key and val until the end of the transaction.
-func (txn *Txn) Set(key, val []byte) error {
-	return txn.SetEntry(NewEntry(key, val))
-}
-
-// SetEntry takes an Entry struct and adds the key-value pair in the struct,
-// along with other metadata to the database.
-//
-// The current transaction keeps a reference to the entry passed in argument.
-// Users must not modify the entry until the end of the transaction.
-func (txn *Txn) SetEntry(e *Entry) error {
-	return txn.modify(e)
-}
-
-// Delete deletes a key.
-//
-// This is done by adding a delete marker for the key at commit timestamp.  Any
-// reads happening before this timestamp would be unaffected. Any reads after
-// this commit would see the deletion.
-//
-// The current transaction keeps a reference to the key byte slice argument.
-// Users must not modify the key until the end of the transaction.
-func (txn *Txn) Delete(key []byte) error {
-	e := &Entry{
-		Key:  key,
-		meta: bitDelete,
-	}
-	return txn.modify(e)
-}
-
 // Get looks for key and returns corresponding Item.
 // If key is not found, ErrKeyNotFound is returned.
 func (txn *Txn) Get(key []byte) (item *Item, rerr error) {
@@ -179,27 +77,6 @@ func (txn *Txn) Get(key []byte) (item *Item, rerr error) {
 	}
 
 	item = new(Item)
-	if txn.update {
-		if e, has := txn.pendingWrites[string(key)]; has && bytes.Equal(key, e.Key) {
-			if isDeletedOrExpired(e.meta, e.ExpiresAt) {
-				return nil, ErrKeyNotFound
-			}
-			// Fulfill from cache.
-			item.meta = e.meta
-			item.val = e.Value
-			item.userMeta = e.UserMeta
-			item.key = key
-			item.status = prefetched
-			item.version = txn.readTs
-			item.expiresAt = e.ExpiresAt
-			// We probably don't need to set db on item here.
-			return item, nil
-		}
-		// Only track reads if this is update txn. No need to track read if txn serviced it
-		// internally.
-		txn.addReadKey(key)
-	}
-
 	seek := y.KeyWithTs(key, txn.readTs)
 	vs, err := txn.db.get(seek)
 	if err != nil {
@@ -222,20 +99,6 @@ func (txn *Txn) Get(key []byte) (item *Item, rerr error) {
 	return item, nil
 }
 
-func (txn *Txn) addReadKey(key []byte) {
-	if txn.update {
-		fp := z.MemHash(key)
-
-		// Because of the possibility of multiple iterators it is now possible
-		// for multiple threads within a read-write transaction to read keys at
-		// the same time. The reads slice is not currently thread-safe and
-		// needs to be locked whenever we mark a key as read.
-		txn.readsLock.Lock()
-		txn.reads = append(txn.reads, fp)
-		txn.readsLock.Unlock()
-	}
-}
-
 // Discard discards a created transaction. This method is very important and must be called. Commit
 // method calls this internally, however, calling this multiple times doesn't cause any issues. So,
 // this can safely be called via a defer right when transaction is created.
@@ -251,200 +114,6 @@ func (txn *Txn) Discard() {
 	txn.discarded = true
 }
 
-func (txn *Txn) commitAndSend() (func() error, error) {
-	commitTs := txn.commitTs
-
-	keepTogether := true
-	setVersion := func(e *Entry) {
-		if e.version == 0 {
-			e.version = commitTs
-		} else {
-			keepTogether = false
-		}
-	}
-	for _, e := range txn.pendingWrites {
-		setVersion(e)
-	}
-	// The duplicateWrites slice will be non-empty only if there are duplicate
-	// entries with different versions.
-	for _, e := range txn.duplicateWrites {
-		setVersion(e)
-	}
-
-	entries := make([]*Entry, 0, len(txn.pendingWrites)+len(txn.duplicateWrites)+1)
-
-	processEntry := func(e *Entry) {
-		// Suffix the keys with commit ts, so the key versions are sorted in
-		// descending order of commit timestamp.
-		e.Key = y.KeyWithTs(e.Key, e.version)
-		// Add bitTxn only if these entries are part of a transaction. We
-		// support SetEntryAt(..) in managed mode which means a single
-		// transaction can have entries with different timestamps. If entries
-		// in a single transaction have different timestamps, we don't add the
-		// transaction markers.
-		if keepTogether {
-			e.meta |= bitTxn
-		}
-		entries = append(entries, e)
-	}
-
-	// The following debug information is what led to determining the cause of
-	// bank txn violation bug, and it took a whole bunch of effort to narrow it
-	// down to here. So, keep this around for at least a couple of months.
-	// var b strings.Builder
-	// fmt.Fprintf(&b, "Read: %d. Commit: %d. reads: %v. writes: %v. Keys: ",
-	// 	txn.readTs, commitTs, txn.reads, txn.conflictKeys)
-	for _, e := range txn.pendingWrites {
-		processEntry(e)
-	}
-	for _, e := range txn.duplicateWrites {
-		processEntry(e)
-	}
-
-	if keepTogether {
-		// CommitTs should not be zero if we're inserting transaction markers.
-		y.AssertTrue(commitTs != 0)
-		e := &Entry{
-			Key:   y.KeyWithTs(txnKey, commitTs),
-			Value: []byte(strconv.FormatUint(commitTs, 10)),
-			meta:  bitFinTxn,
-		}
-		entries = append(entries, e)
-	}
-
-	req, err := txn.db.sendToWriteCh(entries)
-	if err != nil {
-		return nil, err
-	}
-	ret := func() error {
-		err := req.Wait()
-		// Wait before marking commitTs as done.
-		// We can't defer doneCommit above, because it is being called from a
-		// callback here.
-		return err
-	}
-	return ret, nil
-}
-
-func (txn *Txn) commitPrecheck() error {
-	if txn.discarded {
-		return errors.New("Trying to commit a discarded txn")
-	}
-	keepTogether := true
-	for _, e := range txn.pendingWrites {
-		if e.version != 0 {
-			keepTogether = false
-		}
-	}
-
-	// If keepTogether is True, it implies transaction markers will be added.
-	// In that case, commitTs should not be never be zero. This might happen if
-	// someone uses txn.Commit instead of txn.CommitAt in managed mode.  This
-	// should happen only in managed mode. In normal mode, keepTogether will
-	// always be true.
-	if keepTogether && txn.commitTs == 0 {
-		return errors.New("CommitTs cannot be zero. Please use commitAt instead")
-	}
-	return nil
-}
-
-// Commit commits the transaction, following these steps:
-//
-// 1. If there are no writes, return immediately.
-//
-// 2. Check if read rows were updated since txn started. If so, return ErrConflict.
-//
-// 3. If no conflict, generate a commit timestamp and update written rows' commit ts.
-//
-// 4. Batch up all writes, write them to value log and LSM tree.
-//
-// 5. If callback is provided, Badger will return immediately after checking
-// for conflicts. Writes to the database will happen in the background.  If
-// there is a conflict, an error will be returned and the callback will not
-// run. If there are no conflicts, the callback will be called in the
-// background upon successful completion of writes or any error during write.
-//
-// If error is nil, the transaction is successfully committed. In case of a non-nil error, the LSM
-// tree won't be updated, so there's no need for any rollback.
-func (txn *Txn) Commit() error {
-	// txn.conflictKeys can be zero if conflict detection is turned off. So we
-	// should check txn.pendingWrites.
-	if len(txn.pendingWrites) == 0 {
-		return nil // Nothing to do.
-	}
-	// Precheck before discarding txn.
-	if err := txn.commitPrecheck(); err != nil {
-		return err
-	}
-	defer txn.Discard()
-
-	txnCb, err := txn.commitAndSend()
-	if err != nil {
-		return err
-	}
-	// If batchSet failed, LSM would not have been updated. So, no need to rollback anything.
-
-	// TODO: What if some of the txns successfully make it to value log, but others fail.
-	// Nothing gets updated to LSM, until a restart happens.
-	return txnCb()
-}
-
-type txnCb struct {
-	commit func() error
-	user   func(error)
-	err    error
-}
-
-func runTxnCallback(cb *txnCb) {
-	switch {
-	case cb == nil:
-		panic("txn callback is nil")
-	case cb.user == nil:
-		panic("Must have caught a nil callback for txn.CommitWith")
-	case cb.err != nil:
-		cb.user(cb.err)
-	case cb.commit != nil:
-		err := cb.commit()
-		cb.user(err)
-	default:
-		cb.user(nil)
-	}
-}
-
-// CommitWith acts like Commit, but takes a callback, which gets run via a
-// goroutine to avoid blocking this function. The callback is guaranteed to run,
-// so it is safe to increment sync.WaitGroup before calling CommitWith, and
-// decrementing it in the callback; to block until all callbacks are run.
-func (txn *Txn) CommitWith(cb func(error)) {
-	if cb == nil {
-		panic("Nil callback provided to CommitWith")
-	}
-
-	if len(txn.pendingWrites) == 0 {
-		// Do not run these callbacks from here, because the CommitWith and the
-		// callback might be acquiring the same locks. Instead run the callback
-		// from another goroutine.
-		go runTxnCallback(&txnCb{user: cb, err: nil})
-		return
-	}
-
-	// Precheck before discarding txn.
-	if err := txn.commitPrecheck(); err != nil {
-		cb(err)
-		return
-	}
-
-	defer txn.Discard()
-
-	commitCb, err := txn.commitAndSend()
-	if err != nil {
-		go runTxnCallback(&txnCb{user: cb, err: err})
-		return
-	}
-
-	go runTxnCallback(&txnCb{user: cb, commit: commitCb})
-}
-
 // ReadTs returns the read timestamp of the transaction.
 func (txn *Txn) ReadTs() uint64 {
 	return txn.readTs
@@ -455,11 +124,7 @@ func (txn *Txn) Discarded() bool {
 	return txn.discarded
 }
 
-// NewTransaction creates a new transaction. Badger supports concurrent execution of transactions,
-// providing serializable snapshot isolation, avoiding write skews. Badger achieves this by tracking
-// the keys read and at Commit time, ensuring that these read keys weren't concurrently modified by
-// another transaction.
-//
+// NewReadTxn is a read-only transaction.
 // For read-only transactions, set update to false. In this mode, we don't track the rows read for
 // any changes. Thus, any long running iterations done in this mode wouldn't pay this overhead.
 //
@@ -475,35 +140,17 @@ func (txn *Txn) Discarded() bool {
 //  txn := db.NewTransaction(false)
 //  defer txn.Discard()
 //  // Call various APIs.
-//
-// NewTransactionAt follows the same logic as DB.NewTransaction(), but uses the
-// provided read timestamp.
-//
-// This is only useful for databases built on top of Badger (like Dgraph), and
-// can be ignored by most users.
-func (db *DB) NewTransactionAt(readTs uint64, update bool) *Txn {
-	txn := db.newTransaction(update, true)
+func (db *DB) NewReadTxn(readTs uint64) *Txn {
+	txn := db.newTransaction()
 	txn.readTs = readTs
 	return txn
 }
 
-func (db *DB) newTransaction(update, isManaged bool) *Txn {
-	if db.opt.ReadOnly && update {
-		// DB is read-only, force read-only transaction.
-		update = false
-	}
-
+func (db *DB) newTransaction() *Txn {
 	txn := &Txn{
-		update: update,
-		db:     db,
-		count:  1,                       // One extra entry for BitFin.
-		size:   int64(len(txnKey) + 10), // Some buffer for the extra entry.
-	}
-	if update {
-		if db.opt.DetectConflicts {
-			txn.conflictKeys = make(map[uint64]struct{})
-		}
-		txn.pendingWrites = make(map[string]*Entry)
+		db:    db,
+		count: 1,                       // One extra entry for BitFin.
+		size:  int64(len(txnKey) + 10), // Some buffer for the extra entry.
 	}
 	return txn
 }
@@ -515,18 +162,8 @@ func (db *DB) View(fn func(txn *Txn) error) error {
 	if db.IsClosed() {
 		return ErrDBClosed
 	}
-	txn := db.NewTransactionAt(math.MaxUint64, false)
+	txn := db.NewReadTxn(math.MaxUint64)
 	defer txn.Discard()
 
 	return fn(txn)
-}
-
-// Update executes a function, creating and managing a read-write transaction
-// for the user. Error returned by the function is relayed by the Update method.
-// Update cannot be used with managed transactions.
-func (db *DB) Update(fn func(txn *Txn) error) error {
-	if db.IsClosed() {
-		return ErrDBClosed
-	}
-	panic("Update can only be used with managedDB=false.")
 }
